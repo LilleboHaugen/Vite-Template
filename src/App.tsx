1d85c5b40c85f31ce5ecc0c30ffac36edba35a6b
JLH010 <jonas.lillebo.haugen@politiet.no>
2025-02-05 18:46:46 +0100

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ interface AppProps {
   // Define your props here
 }
 
+let test = "Hello"
+
+test = 10
+
+console.log(test)
+
 export const App: React.FC<AppProps> = () => {
   return <div className="App">App</div>
 }
